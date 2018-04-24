24d6a41a227cf3175c64d4b666317cb08af60e5e
Ziontech <junl@wavity.com>
2018-04-24 14:15:02 -0700

    Building a ToDo application

--- app/driver.js
+++ app/driver.js
@@ -1,10 +1,25 @@
+var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
 
-var Helloworld = Marionette.LayoutView.extend({
+
+var TodoList = Marionette.LayoutView.extend({
   el: '#app-hook',
   template: require('./templates/layout.html')
 });
 
-var hello = new Helloworld();
+var todo = new TodoList({
+  model: new Backbone.Model({
+    items: [
+      {
+        assignee: 'Scott',
+        text: 'Write a book about Marionette'
+      },
+      {
+        assignee: 'Andrew',
+        text: 'Do some coding'
+      }
+    ]
+  })
+});
 
-hello.render();
+todo.render();
